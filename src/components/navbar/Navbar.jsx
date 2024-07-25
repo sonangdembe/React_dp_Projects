dd5872b82bc7d0a42a9c4830b07ab813ff618a22
Sona Angdembe <asonaxaviersm1@gmail.com>
2024-07-24 22:15:32 -0700

    register

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,9 +4,6 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-    
-
-
 
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
--- src/pages/blog/components/form/Form.jsx
+++ src/pages/blog/components/form/Form.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const Form = ({type}) => {
 
+	
   return (
     <div class="flex justify-center  w-screen h-screen">
 
